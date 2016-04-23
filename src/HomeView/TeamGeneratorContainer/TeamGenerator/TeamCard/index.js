b63eabb06f1ef98e834184a14d23986e069bc6ac
Daniel Lowman <dnlowman@gmail.com>
2016-04-23 18:49:09 +0100

    Team members are now displayed in a list.

--- src/HomeView/TeamGeneratorContainer/TeamGenerator/TeamCard/index.js
+++ src/HomeView/TeamGeneratorContainer/TeamGenerator/TeamCard/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const TeamCard = ({ players }) => {
+    let playerList = players.map(player => <li>{player}</li>);
+
     return (
         <div className="card">
             <div className="content">
                 <div className="header">Team:</div>
                 <div className="description">
                     <ul>
-                        <li>
-                            {players}
-                        </li>
+                        {playerList}
                     </ul>
                 </div>
             </div>
